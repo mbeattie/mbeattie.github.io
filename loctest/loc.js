59b1937443f328c04ea66f0043c4d9d504d79e63
Michael Beattie <mtbeedee@gmail.com>
2024-12-06 13:54:28 -0500

    Added example of location API

---
+++ loctest/loc.js
@@ -0,0 +1,18 @@
+
+const x = document.getElementById("loc");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+
+document.getElementById("get_loc").addEventListener("click", () => {
+    getLocation(); 
+});
